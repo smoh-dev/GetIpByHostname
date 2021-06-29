5a04edc8b57e8934b28c285ba5cba359eb5da9c2
smoh <seungmuk92@gmail.com>
2021-06-29 13:32:24 +0900

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace GetIpByHostname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostname = "";
+            IPAddress[] listIPAddress = Dns.GetHostAddresses(hostname);
+            IPAddress[] listIPV4 = listIPAddress.Where(x=>x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToArray();
+            Console.WriteLine($"Input Hostname: {hostname}");
+            Console.WriteLine($"IP Address: {listIPAddress[0]}");
+        }
+    }
+}
